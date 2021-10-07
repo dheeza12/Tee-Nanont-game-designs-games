667768ada5d59593952913ae681720866b95318e
dheeza12 <teena@kkumail.com>
2021-10-07 14:21:31 +0700

    orange ge

---
+++ Orange good/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1633590359,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/solidground-sheet0.png",
+		"images/food-sheet0.png",
+		"images/startbutton-sheet0.png",
+		"images/leftarrow-sheet0.png",
+		"images/uparrow-sheet0.png",
+		"images/rightarrow-sheet0.png",
+		"images/replaybutton-sheet0.png",
+		"images/rename-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bgbuilding-sheet0.png",
+		"media/melodyloops-original-best-holidays-2m30s.m4a",
+		"media/melodyloops-original-best-holidays-2m30s.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
